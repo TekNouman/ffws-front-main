fc5eb20f4f66763e2a13a308815a3758808523f6
TekNouman <89212235+TekNouman@users.noreply.github.com>
2021-09-07 14:07:48 +0500

    BenchPlayer

--- src/components/lists/RosterBenchPlayer/RosterBenchPlayer.tsx
+++ src/components/lists/RosterBenchPlayer/RosterBenchPlayer.tsx
@@ -95,7 +95,7 @@ class RosterBenchPlayer extends React.Component{
   
   render(){
     
-    const {data} = this.props;
+    const data = this.props;
    
     
 
@@ -109,7 +109,7 @@ class RosterBenchPlayer extends React.Component{
 
           <tbody>
 
-            <TableCellRosterPlayer {...data} data={data}/>
+            <TableCellRosterPlayer {...data} />
 
           </tbody>
         </Table>
