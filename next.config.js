72dfc15022adb6a0c8e2013ff41fb470b621f1f2
TekNouman <89212235+TekNouman@users.noreply.github.com>
2021-09-07 16:11:11 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 const withImages = require('next-images');
 const withVideos = require('next-videos');
 
+
+module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+}
+
 module.exports = withImages(
     withVideos({
         images: {
